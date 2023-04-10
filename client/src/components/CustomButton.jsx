822d14ed4792aa9fd68bf83dc1d02ddb058227ea
Alex <alexferreira.af411@gmail.com>
2023-04-10 08:11:03 -0300

    feat: novo projeto camiseta

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,29 @@
+import { color } from 'framer-motion'
+import React from 'react'
+import state from '../store';
+import { useSnapshot } from 'valtio';
+
+const CustomButton = ({ type, title, customStyLes, handLeClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if(type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff'
+      }
+    }
+  }
+
+  return (
+    <button
+      className={'px-2 py-1.5 flex-1 rounded-md ${customStyles}'}
+      style={generateStyle(type)}
+      onClick={handLeClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
